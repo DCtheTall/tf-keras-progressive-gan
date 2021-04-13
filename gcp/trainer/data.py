0d44d0b74b79646b852c9e8e85479febddd3eaca
DCtheTall <dylancutler@google.com>
2021-04-13 18:45:28 -0400

    add code to generate training data iterator

--- gcp/trainer/data.py
+++ gcp/trainer/data.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def load_image_dataset(image_dir, batch_size, image_size):
+def load_image_dataset(image_dir, image_size, batch_size):
   """Load the images into a dataset from the given path."""
   return tf.keras.preprocessing.image_dataset_from_directory(
       'data',
@@ -15,3 +15,23 @@ def augment_and_scale(x):
   """Image augmentation and scaling."""
   x = tf.image.random_flip_left_right(x)
   return tf.cast(x, tf.float32) / 255.0
+
+
+def images_as_np_iter(image_dir, image_size, batch_size):
+  """Create a NumPy iterator out of the images in the training set."""
+  X_train_ds = load_image_dataset(image_dir, image_size, batch_size)
+  return X_train_ds.as_numpy_iterator()
+
+
+def training_data(image_dir, image_size, batch_size):
+  """Return the training set iterator, which can be used for multiple training epochs."""
+  X_train = images_as_np_iter(image_dir, image_size, batch_size)
+  while True:
+    try:
+      X_batch = next(X_train)
+      if len(X_batch) != batch_size:
+        X_train = images_as_np_iter(image_dir, image_size, batch_size)
+        X_batch = next(X_train)
+      yield X_batch
+    except StopIteration:
+      X_train = images_as_np_iter(image_dir, image_size, batch_size)
