6d35d30c72265bb20527b7a527ae5fe4398ac97f
DCtheTall <dylancutler@google.com>
2021-04-14 09:23:51 -0400

    fix loading data from GCS

--- gcp/progan/trainer/data.py
+++ gcp/progan/trainer/data.py
@@ -1,14 +1,32 @@
 import tensorflow as tf
+from tensorflow.python.platform import gfile
+
+
+# Cache images in memory so we don't have to load them from GCS each time.
+image_cache = {}
 
 
 def load_image_dataset(image_dir, image_size, batch_size):
   """Load the images into a dataset from the given path."""
-  return tf.keras.preprocessing.image_dataset_from_directory(
-      'data',
-      label_mode=None,
-      batch_size=batch_size,
-      interpolation='nearest',
-      image_size=(image_size, image_size)).map(augment_and_scale)
+  data_gen = lambda: data_generator(image_dir, image_size)
+  ds = tf.data.Dataset.from_generator(
+      data_gen, output_signature=tf.TensorSpec(
+          shape=(image_size, image_size, 3), dtype=tf.float32))
+  ds = ds.shuffle(batch_size, reshuffle_each_iteration=True)
+  return ds.batch(batch_size).map(augment_and_scale).repeat()
+
+
+def data_generator(image_dir, image_size):
+  """Generator for loading image data."""
+  for fname in gfile.ListDirectory(image_dir):
+    if fname in image_cache:
+        yield image_cache[fname]
+        continue
+    img = tf.image.decode_png(
+        tf.io.read_file(os.path.join(image_dir, fname))).numpy()
+    img = tf.image.resize(img, (image_size,) * 2, method='nearest')
+    image_cache[fname] = img
+    yield img
 
 
 def augment_and_scale(x):
@@ -27,11 +45,7 @@ def training_data(image_dir, image_size, batch_size):
   """Return the training set iterator, which can be used for multiple training epochs."""
   X_train = images_as_np_iter(image_dir, image_size, batch_size)
   while True:
-    try:
-      X_batch = next(X_train)
-      if len(X_batch) != batch_size:
-        X_train = images_as_np_iter(image_dir, image_size, batch_size)
-        X_batch = next(X_train)
-      yield X_batch
-    except StopIteration:
-      X_train = images_as_np_iter(image_dir, image_size, batch_size)
+     X_batch = next(X_train)
+     if len(X_batch) != batch_size:
+       X_batch = next(X_train)
+     yield X_batch
