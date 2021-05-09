f128a4daf6f3f8a2fa64a7d62fddc1c748aab284
DCtheTall <dylancutler@google.com>
2021-05-09 17:27:39 -0400

    reduce logging frequency during training

--- gcp/progan/trainer/task.py
+++ gcp/progan/trainer/task.py
@@ -108,7 +108,7 @@ if __name__ == '__main__':
   parser.add_argument('--print_every_n_batches',
                       help='Logs progress every N training batches',
                       type=int,
-                      default=50)
+                      default=200)
   parser.add_argument('--save_every_n_batches',
                       help='Saves model every N training batches',
                       type=int,
